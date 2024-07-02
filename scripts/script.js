820def9239c7c4c3f3b82fa30b6af4eb12bd939a
Simo Schmidt <simosch7@gmail.com>
2024-07-02 16:17:57 +0300

    pooleli 16:18 02.07.2024

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,43 @@ function getComputerChoice () {
   let randomNumber = Math.floor(Math.random () * 3) + 1;
   switch (randomNumber) {
     case 1:
-      computerChoice = "rock";
-      console.log(computerChoice);
+      computerChoice = 'rock';
       break;
     case 2:
-      computerChoice = "paper";
-      console.log(computerChoice);
+      computerChoice = 'paper';
       break;
     case 3:
-      computerChoice = "scissors";
-      console.log(computerChoice);
-      break
+      computerChoice = 'scissors';
+      break;
   }
-  return randomNumber;
+  console.log('computer chose ' + computerChoice);
+  console.log(randomNumber);
+//  return randomNumber;
 }
 
-getComputerChoice();
+function getHumanChoice () {
+  let userInput = prompt('Rock, paper or scissors?');
+  userInput = userInput.toLocaleLowerCase();
+  if (userInput != "rock" || userInput != "paper" || userInput != "scissors") {
+    prompt('That\'s not one of the options :) Please select Rock, paper or scissors');
+  }
+  /*
+  switch (userInput.toLocaleLowerCase()) {
+    case "rock":
+      humanChoice = 'rock';
+      break;
+    case "paper":
+      humanChoice = 'paper';
+      break;
+    case "scissors":
+      humanChoice = 'scissors';
+      break;
+  }
+  console.log('user chose ' + humanChoice);
+  return userInput;
+  */
+  console.log('user chose ' + userInput);
+}
+
+getComputerChoice ();
+getHumanChoice ();
