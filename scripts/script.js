5d360ffc03a04b2586703c03df39ad579b470b27
Simo Schmidt <simosch7@gmail.com>
2024-07-02 15:30:47 +0300

    Write a function getComputerChoice that randomly returns “rock”, “paper” or “scissors”.

--- scripts/script.js
+++ scripts/script.js
@@ -3,27 +3,20 @@
 function getComputerChoice () {
   let randomNumber = Math.floor(Math.random () * 3) + 1;
   switch (randomNumber) {
-    case: 1:
+    case 1:
       computerChoice = "rock";
+      console.log(computerChoice);
+      break;
+    case 2:
+      computerChoice = "paper";
+      console.log(computerChoice);
+      break;
+    case 3:
+      computerChoice = "scissors";
+      console.log(computerChoice);
+      break
   }
   return randomNumber;
 }
 
-console.log(getComputerChoice());
-
-switch (foo) {
-  case -1:
-    console.log("negative 1");
-    break;
-  case 0: // Value of foo matches this criteria; execution starts from here
-    console.log(0);
-  // Forgotten break! Execution falls through
-  case 1: // no break statement in 'case 0:' so this case will run as well
-    console.log(1);
-    break; // Break encountered; will not continue into 'case 2:'
-  case 2:
-    console.log(2);
-    break;
-  default:
-    console.log("default");
-}
+getComputerChoice();
