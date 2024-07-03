eb99177176944d8a264475a79506c1bab4ac6b78
simoschmidt <simosch7@gmail.com>
2024-07-03 13:45:35 +0300

    Write a function playRound that plays a single round

--- scripts/script.js
+++ scripts/script.js
@@ -26,19 +26,24 @@ function getHumanChoice () {
     humanChoice = prompt ('That\'s not one of the options :) Please select Rock, paper or scissors')
     humanChoice = humanChoice.toLocaleLowerCase ();
   }
-  console.log('user chose ' + humanChoice);
+  console.log ('user chose ' + humanChoice);
   return humanChoice;
 }
 
 function playRound (humanChoice, computerChoice) {
   if ((humanChoice == 'rock' && computerChoice == 'paper') || 
-  (humanChoice == 'paper' && computerChoice == 'scissors') || (humanChoice == 'scissors' && computerChoice == 'rock')) {
-      console.log ('You lose! ' + computerChoice + ' beats ' + humanChoice + '.')
+  (humanChoice == 'paper' && computerChoice == 'scissors') || 
+  (humanChoice == 'scissors' && computerChoice == 'rock')) {
+      computerScore ++;
+      console.log ('You lose! ' + computerChoice[0].toUpperCase() + computerChoice.slice(1) + ' beats ' + humanChoice + '.');
+    } else if (humanChoice === computerChoice) {
+      console.log ('Draw!');
     } else {
-      console.log ('huh');
+      humanScore ++;
+      console.log ('You win! ' + humanChoice[0].toUpperCase() + humanChoice.slice(1) + ' beats ' + computerChoice + '.');
     }
-    console.log(humanChoice)
-    console.log(computerChoice)
+    console.log (humanChoice)
+    console.log (computerChoice)
 }
 
 const computerSelection = getComputerChoice ();
