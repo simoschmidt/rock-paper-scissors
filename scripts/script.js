225883cc77a571bebd02304eb1bd26ae1a660a6b
Simo Schmidt <simosch7@gmail.com>
2024-07-16 14:32:28 +0300

    Add results tracking and display on page

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
 let drawScore = 0;
+const resultsDiv = document.querySelector("#results");
 
 function getComputerChoice () {
   let randomNumber = Math.floor(Math.random () * 3) + 1;
@@ -16,7 +17,7 @@ function getComputerChoice () {
       computerChoice = 'scissors';
       break;
   }
-  console.log ('Computer chose ' + computerChoice + '.');
+ //console.log ('Computer chose ' + computerChoice + '.');
   return computerChoice;
 }
 
@@ -28,46 +29,49 @@ function playGame () {
     (humanChoice == 'paper' && computerChoice == 'scissors') || 
     (humanChoice == 'scissors' && computerChoice == 'rock')) {
         computerScore ++;
-        console.log ('You lose! ' + computerChoice[0].toUpperCase() +
-         computerChoice.slice(1) + ' beats ' + humanChoice + '.');
-        console.log (humanScore + computerScore + drawScore);
+        resultsDiv.textContent = 'You chose ' + humanChoice +
+          ' and the computer chose ' + computerChoice +'. You lose! ' +
+          computerChoice[0].toUpperCase() + computerChoice.slice(1) +
+          ' beats ' + humanChoice + '. The current score is You: ' +
+          humanScore + ' / Computer: ' + computerScore + ' / Draws: ' +
+          drawScore;
       } else if (humanChoice === computerChoice) {
         drawScore ++;
-        console.log ('Draw!');
-        console.log (humanScore + computerScore + drawScore);
+        resultsDiv.textContent = 'You chose ' + humanChoice +
+        ' and the computer chose ' + computerChoice +
+        '. Draw! The current score is You: ' + humanScore + ' / Computer: ' +
+        computerScore + ' / Draws: ' + drawScore;
       } else {
         humanScore ++;
-        console.log ('You win! ' + humanChoice[0].toUpperCase() +
-         humanChoice.slice(1) + ' beats ' + computerChoice + '.');
-        console.log (humanScore + computerScore + drawScore);
+        resultsDiv.textContent = 'You chose ' + humanChoice +
+        ' and the computer chose ' + computerChoice +'. You win! ' +
+        humanChoice[0].toUpperCase() + humanChoice.slice(1) + ' beats ' +
+        computerChoice + '. The current score is You: ' + humanScore +
+        ' / Computer: ' + computerScore + ' / Draws: ' + drawScore;
       }
-    console.log('---')
-
+    //console.log('---')
     checkScore();
   }
   
   document.addEventListener("click", function(event) {
     switch (event.target.id) {
       case "rockBtn":
-        console.log ('Human chose rock.');
+        //console.log ('Human chose rock.');
         playRound('rock');
         break;
       case "paperBtn":
-        console.log ('Human chose paper.');
+        //console.log ('Human chose paper.');
         playRound('paper');
         break;
       case "scissorsBtn":
-        console.log ('Human chose scissors.');
+        //console.log ('Human chose scissors.');
         playRound('scissors');
         break;
     }
-
   }); 
-
 }
 
 function checkScore() {
-  const resultsDiv = document.querySelector("#results");
   if (humanScore + computerScore + drawScore == 5) {
     document.getElementById("rockBtn").disabled = true;
     document.getElementById("paperBtn").disabled = true;
